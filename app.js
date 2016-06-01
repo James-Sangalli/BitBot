4490003139e262760c830347cbf6529468b9f2d1
James Sangalli <j.l.sangalli@gmail.com>
2016-06-01 18:20:31 +1200

    added buy and sell functions

--- app.js
+++ app.js
@@ -1,66 +1,75 @@
+//modules
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var request = require("superagent");
-var price;
-var amount;
-var sellPrice;
-var buyPrice;
 var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+//global variables
+var price,
+amount,
+sellPrice,
+buyPrice,
+sold = false,
+bought = false
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(8080,  () => {
-  console.log('listening on port ', 8080);
-});
-
 getPrice();
+askUser("How much bitcoin would you like to trade from your account?","amount");
 
 function getPrice(){
   setTimeout(() => {
     request.get("https://blockchain.info/ticker",(err,data) => {
-      console.log("Bitcoin price: $NZD", data.body.NZD.buy);
       price = data.body.NZD.buy;
+      console.log("\nBitcoin price: $NZD", data.body.NZD.buy);
+      if(price == buyPrice && !bought) buy()
+      if(price == sellPrice && !sold) sell()
       getPrice();
     })
   },5000)
 }
 
-//
-// rl.question("How much bitcoin would you like the bot to trade with?", (answer) => {
-//   console.log("You Selected: ", answer);
-//   amount = answer;
-//   rl.close();
-// });
-//
-// rl.question("What price would you like to buy at?", (answer) => {
-//   console.log("You Selected: ", answer);
-//   buyPrice = answer;
-//   rl.close();
-// });
-//
-// rl.question("What price would you like to sell at?", (answer) => {
-//   console.log("You Selected: ", answer);
-//   sellPrice = answer;
-//   rl.close();
-// });
-//
-// var bought = false;
-//
-// function buy(){
-//
-// }
-//
-// var sold = false;
-//
-// function sell(){
-//
-// }
+function askUser(question,variable){
 
+  rl.question(question + ": ", (answer) => {
+    console.log("You Selected: ", answer);
+    if(variable == "amount"){
+      amount = answer;
+      askUser("what would you like to buy at? (in $NZD)", "buyPrice");
+    }
+    else if(variable == "buyPrice"){
+      buyPrice = answer;
+      askUser("what would you like to sell at? (in $NZD)", "sellPrice");
+    }
+    else{
+      sellPrice = answer;
+      askUser("hit ctrl c to abort", null);
+    }
+    // rl.close();
+  });
+}
+
+function buy(){
+  console.log("Buying " + amount + " bitcoin(s) at the price of $" + buyPrice)
+  trade(amount,"buy");
+  bought = true; //stops buying amount of bitcoin but allows you to sell
+  sold = false;
+}
+
+function sell(){
+  console.log("Selling " + amount + " bitcoin(s) at the price of $" + sellPrice)
+  trade(amount,"sell");
+  sold = true; //stops selling but allows buying
+  bought = false;
+}
+
+function trade(amount,type){
+  console.log("trade initiated!");
+}
 
 module.exports = app;
